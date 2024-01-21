df2dcd787aff88fe894bed3c746dee6221923dbe
Stiven Medina <stvnm33@gmail.com>
2024-01-21 09:53:52 -0500

    Creando proyecto base

---
+++ src/main/java/com/pragma/plazoletamicroservicio/MicroservicioDePlazoletaApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.plazoletamicroservicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioDePlazoletaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioDePlazoletaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pragma/plazoletamicroservicio/MicroservicioDePlazoletaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.plazoletamicroservicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioDePlazoletaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
