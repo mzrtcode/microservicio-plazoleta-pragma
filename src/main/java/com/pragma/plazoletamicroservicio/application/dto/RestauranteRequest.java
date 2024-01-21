411706f68a97c9bc6effaf9fd91115a09989b48a
Stiven Medina <stvnm33@gmail.com>
2024-01-21 13:00:13 -0500

    Creando entidades DTO para Restaurante

--- src/main/java/com/pragma/plazoletamicroservicio/application/dto/RestauranteRequest.java
+++ src/main/java/com/pragma/plazoletamicroservicio/application/dto/RestauranteRequest.java
@@ -1,2 +1,26 @@
-package com.pragma.plazoletamicroservicio.application.dto;public class RestauranteRequest {
+package com.pragma.plazoletamicroservicio.application.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RestauranteRequest {
+
+    private Long id;
+
+    private String nombre;
+
+    private String direccion;
+
+    private Long idPropietario;
+
+    private String telefono;
+
+    private String urlLogo;
+
+    private String nit;
 }
