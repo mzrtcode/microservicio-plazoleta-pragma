afdac08508428b4a4d8997a01aa1bbd9aacf3d7d
Stiven Medina <stvnm33@gmail.com>
2024-01-21 11:58:50 -0500

    Añado las implementaciones casos de uso

--- src/main/java/com/pragma/plazoletamicroservicio/dominio/usecae/CategoriaUseCase.java
+++ src/main/java/com/pragma/plazoletamicroservicio/dominio/usecae/CategoriaUseCase.java
@@ -1,2 +1,39 @@
-package com.pragma.plazoletamicroservicio.dominio.usecae;public class CategoriaUseCase {
+package com.pragma.plazoletamicroservicio.dominio.usecae;
+
+import com.pragma.plazoletamicroservicio.dominio.api.ICategoriaServicePort;
+import com.pragma.plazoletamicroservicio.dominio.model.Categoria;
+import com.pragma.plazoletamicroservicio.dominio.spi.ICategoriaPersistencePort;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class CategoriaUseCase implements ICategoriaServicePort {
+
+    private final ICategoriaPersistencePort categoriaPersistencePort;
+
+    @Override
+    public void saveCategoria(Categoria categoria) {
+        categoriaPersistencePort.saveCategoria(categoria);
+    }
+
+    @Override
+    public List<Categoria> getAllCategorias() {
+        return categoriaPersistencePort.getAllCategorias();
+    }
+
+    @Override
+    public Categoria getCategoriaById(Long id) {
+        return categoriaPersistencePort.getCategoriaById(id);
+    }
+
+    @Override
+    public void updateCategoria(Categoria categoria) {
+        categoriaPersistencePort.updateCategoria(categoria);
+    }
+
+    @Override
+    public void deleteCategoria(Long id) {
+        categoriaPersistencePort.deleteCategoria(id);
+    }
 }
--- src/main/java/com/pragma/plazoletamicroservicio/dominio/usecae/RestauranteUseCase.java
+++ src/main/java/com/pragma/plazoletamicroservicio/dominio/usecae/RestauranteUseCase.java
@@ -1,2 +1,39 @@
-package com.pragma.plazoletamicroservicio.dominio.usecae;public class RestauranteUseCase {
+package com.pragma.plazoletamicroservicio.dominio.usecae;
+
+import com.pragma.plazoletamicroservicio.dominio.api.IRestauranteServicePort;
+import com.pragma.plazoletamicroservicio.dominio.model.Restaurante;
+import com.pragma.plazoletamicroservicio.dominio.spi.IRestaurantePersistencePort;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class RestauranteUseCase implements IRestauranteServicePort {
+
+
+    private final IRestaurantePersistencePort restaurantePersistencePort;
+    @Override
+    public void saveRestaurante(Restaurante restaurante) {
+        restaurantePersistencePort.saveRestaurante(restaurante);
+    }
+
+    @Override
+    public List<Restaurante> getAllRestaurantes() {
+        return restaurantePersistencePort.getAllRestaurantes();
+    }
+
+    @Override
+    public Restaurante getRestauranteById(Long id) {
+        return restaurantePersistencePort.getRestauranteById(id);
+    }
+
+    @Override
+    public void updateRestaurante(Restaurante restaurante) {
+        restaurantePersistencePort.updateRestaurante(restaurante);
+    }
+
+    @Override
+    public void deleteRestaurante(Long id) {
+        restaurantePersistencePort.deleteRestaurante(id);
+    }
 }
