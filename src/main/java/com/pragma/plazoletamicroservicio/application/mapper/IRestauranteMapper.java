47ba745293f8d89548e7dca71f1709ee620b198d
Stiven Medina <stvnm33@gmail.com>
2024-01-21 13:00:31 -0500

    Añadiendo interfaz mapper para Restaurante

--- src/main/java/com/pragma/plazoletamicroservicio/application/mapper/IRestauranteMapper.java
+++ src/main/java/com/pragma/plazoletamicroservicio/application/mapper/IRestauranteMapper.java
@@ -1,2 +1,20 @@
-package com.pragma.plazoletamicroservicio.application.mapper;public interface IRestauranteMapper {
+package com.pragma.plazoletamicroservicio.application.mapper;
+
+import com.pragma.plazoletamicroservicio.application.dto.RestauranteRequest;
+import com.pragma.plazoletamicroservicio.application.dto.RestauranteResponse;
+import com.pragma.plazoletamicroservicio.dominio.model.Restaurante;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        unmappedSourcePolicy = ReportingPolicy.IGNORE)
+public interface IRestauranteMapper {
+
+    Restaurante toRestaurante(RestauranteRequest restauranteRequest);
+    RestauranteRequest toRestauranteRequest  (Restaurante restaurante);
+
+    Restaurante toRestaurante(RestauranteResponse restauranteResponse);
+    RestauranteResponse toRestauranteResponse (Restaurante restaurante);
+
 }
