6328413efd4f80ded2dce8aeba85f796c71facaf
Stiven Medina <stvnm33@gmail.com>
2024-01-28 21:49:50 -0500

    Creando el JwtAuthenticationFilter y JwtAuthenticationManager

---
+++ src/main/java/com/pragma/plazoletamicroservicio/infrastructure/security/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,2 @@
+package com.pragma.plazoletamicroservicio.infrastructure.security.jwt;public class JwtAuthenticationFilter {
+}
---
+++ src/main/java/com/pragma/plazoletamicroservicio/infrastructure/security/jwt/JwtAuthenticationManager.java
@@ -0,0 +1,7 @@
+package com.pragma.plazoletamicroservicio.infrastructure.security.jwt;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService_JwtAuthenticationProvider {
+}
