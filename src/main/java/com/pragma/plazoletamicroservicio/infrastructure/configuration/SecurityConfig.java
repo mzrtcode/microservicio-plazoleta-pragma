28717b9edc1472d1b88f5f381b8385188934b84e
Stiven Medina <stvnm33@gmail.com>
2024-02-05 15:59:03 -0500

    Añadiendo la documentacion de la api con SpringDoc

--- src/main/java/com/pragma/plazoletamicroservicio/infrastructure/configuration/SecurityConfig.java
+++ src/main/java/com/pragma/plazoletamicroservicio/infrastructure/configuration/SecurityConfig.java
@@ -4,7 +4,6 @@ import com.pragma.plazoletamicroservicio.infrastructure.security.jwt.JwtAuthenti
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -25,6 +24,7 @@ public class SecurityConfig {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/webjars/swagger-ui/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(sessionManager -> sessionManager
